c42e0d59e24401b2c9f00eb7faeb9a108ce09026
inti0323 <inti0323@DESKTOP-B2JC298>
2018-03-29 12:00:55 +0200

    Lise : creation de la classe produit

--- 03_ECommerce/src/fr/adaming/model/Produit.java
+++ 03_ECommerce/src/fr/adaming/model/Produit.java
@@ -1,9 +1,111 @@
 package fr.adaming.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="produits")
 public class Produit {
-	
-	
+
+	// déclaration des attributs
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id_p")
 	private int idProduit;
-	
+	private String designation;
+	private String description;
+	private Double prix;
+	private int quantite;
+	private boolean selectionne;
+	private byte[] photo;
+
+	// déclaration des constructeurs
+	public Produit() {
+		super();
+	}
+
+	public Produit(String designation, String description, Double prix, int quantite, boolean selectionne,
+			byte[] photo) {
+		super();
+		this.designation = designation;
+		this.description = description;
+		this.prix = prix;
+		this.quantite = quantite;
+		this.selectionne = selectionne;
+		this.photo = photo;
+	}
+
+	public Produit(int idProduit, String designation, String description, Double prix, int quantite,
+			boolean selectionne, byte[] photo) {
+		super();
+		this.idProduit = idProduit;
+		this.designation = designation;
+		this.description = description;
+		this.prix = prix;
+		this.quantite = quantite;
+		this.selectionne = selectionne;
+		this.photo = photo;
+	}
+
+	// déclaration des getters et setters
+	public int getIdProduit() {
+		return idProduit;
+	}
+
+	public void setIdProduit(int idProduit) {
+		this.idProduit = idProduit;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrix() {
+		return prix;
+	}
+
+	public void setPrix(Double prix) {
+		this.prix = prix;
+	}
+
+	public int getQuantite() {
+		return quantite;
+	}
+
+	public void setQuantite(int quantite) {
+		this.quantite = quantite;
+	}
+
+	public boolean isSelectionne() {
+		return selectionne;
+	}
+
+	public void setSelectionne(boolean selectionne) {
+		this.selectionne = selectionne;
+	}
+
+	public byte[] getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(byte[] photo) {
+		this.photo = photo;
+	}
 
 }
